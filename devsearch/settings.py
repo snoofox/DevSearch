780c010d897e8cbfc322162cd288164e1f1bd58a
Ashwin <81082301+snoofox@users.noreply.github.com>
2022-07-25 14:48:18 +0530

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,9 +152,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'shoto.dx@gmail.com'
-EMAIL_HOST_USER = 'shoto.dx@gmail.com'
-EMAIL_HOST_PASSWORD = 'fztcllkahjikpdhr'
+DEFAULT_FROM_EMAIL = os.environ['FROM_EMAIL']
+EMAIL_HOST_USER = os.environ['HOST_EMAIL']
+EMAIL_HOST_PASSWORD = os.environ['HOST_PASS']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
